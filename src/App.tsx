8cdf2ef5b5a35dd800ab0255d8b9bc1c8e4345c6
ambi-valentia <114367732+ambi-valentia@users.noreply.github.com>
2024-08-21 05:21:34 +0300

    feat: add msg for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from "react";
 import { PageIndex } from "./pages/index";
+import "./App.css"
 
 function App() {
-  return (
+  const width = window.innerWidth;
+  return width > 700 ? (
     <div className="App">
       <PageIndex />
+    </div>
+  ) : (
+    <div className="mobile">
+      ПРОСТИТЕ! НО ДЛЯ МОБИЛЬНЫХ ТЕЛЕФОНОВ У НАС ЕСТЬ МОБИЛЬНОЕ ПРИЛОЖЕНИЕ
     </div>
   );
 }
